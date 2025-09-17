8c30acd89e10fcdf0ea2bdd1071b0ef1a12a204e
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-09-18 02:12:40 +0600

    fixed

--- app/component/Header.jsx
+++ app/component/Header.jsx
@@ -9,27 +9,27 @@ const Header = () => {
         <Container className={'md:flex items-center justify-between py-2.5'}>
             <div className="left flex items-center gap-10">
                
-                    <p className='text-white text-[12px] flex items-center gap-2 font-raleway'>
+                    <p className='text-white text-[10px] md:text-[12px] flex items-center gap-2 font-raleway'>
                     <FaLocationDot className='w-5 h-5 text-white' />
 
                     Your Address Here
                     </p>
                     
                 
-                    <p className='text-white text-[12px] flex items-center gap-2 font-poppins'>
+                    <p className='text-white text-[10px] md:text-[12px] flex items-center gap-2 font-poppins'>
                     <MdCall className='w-5 h-5 text-white' />
                         +1234567890
                     </p>
                     
                 
-                    <p className='text-white text-[12px] flex items-center gap-2 font-raleway'>
+                    <p className='text-white text-[10px] md:text-[12px] flex items-center gap-2 font-raleway'>
                     <MdEmail className='w-5 h-5 text-white'/>
                         example@example.com
                     </p>
                     
             </div>
 
-            <div className="right md:w-[130px] list-none flex items-center gap-5 mt-2 md:mt-0">
+            <div className="right md:w-[130px] list-none flex items-center justify-center md: gap-5 mt-2 md:mt-0">
                <li> 
                 <a href="#">
                     <FaFacebookF className='w-[9.2px] h-[20px] text-white' />
